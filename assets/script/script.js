c0d371000dfc6da7b7e1c77c7f57af88c5ee92c4
eacquistapace <eacquistapace@gmail.com>
2022-05-11 21:53:03 -0600

    images file added, img section added to html, img array started in js

---
+++ assets/script/script.js
@@ -0,0 +1,16 @@
+var characterArray = [
+  {
+    img: "./assets/images/trafalgar-law.jpg",
+    desc: "Trafalgar Law",
+  },
+
+  {
+    img: "./assets/images/kilala.jpg",
+    desc: "Kilala",
+  },
+
+  {
+    img: "./assets/images/miki-kuroda.jpg",
+    desc: "Miki Kuroda",
+  },
+];
