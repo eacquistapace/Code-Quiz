770fbffb01fa47bdcf20bdae8cb67f9dd7d4674d
eacquistapace <eacquistapace@gmail.com>
2022-05-11 22:57:06 -0600

    high scores id added

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,6 +1,11 @@
+//Variable declarations
+var gameCard = $("#game-card");
 var characterImage = $("#character-img");
 var characterDesc = $("#character-desc");
 var timerEl = $("#game-timer");
+var startButton = $("#start-button");
+var highScoresList = $("#high-scores");
+//create option 1, 2, 3 var buttons for game answers?
 
 //Imgs and img descriptions array
 var characterArray = [
@@ -19,3 +24,7 @@ var characterArray = [
     desc: "Miki Kuroda",
   },
 ];
+
+//Logic variables
+var timer = 5;
+var currentIndex = 0;
